eb26dee02ca3ff3504b068cc42c0faa4da6f19c0
RAMAHALISON Antoniaina <arisehenoantoniaina@gmail.com>
2025-07-19 20:35:44 +0300

    fix(security): move JWT secret to .env

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,14 +3,21 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     PrismaModule,
-    JwtModule.register({
-      secret: 'mypersonalsupersecretkeyex',
-      signOptions: { expiresIn: '1d' }
-    })
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async ( configService: ConfigService ) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_EXPIRES_IN') || '1h',
+        }
+      })
+    }),
   ],
   providers: [AuthService],
   controllers: [AuthController]
